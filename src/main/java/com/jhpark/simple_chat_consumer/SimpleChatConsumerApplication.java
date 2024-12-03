190cd7d8b0e4494406af7c8938624624cde095cc
박지훈 <qkrwlgns0510@gmail.com>
2024-12-03 17:24:28 +0900

    first commit

---
+++ src/main/java/com/jhpark/simple_chat_consumer/SimpleChatConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jhpark.simple_chat_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhpark/simple_chat_consumer/SimpleChatConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.simple_chat_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
