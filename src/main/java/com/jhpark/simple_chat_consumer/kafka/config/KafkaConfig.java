0dfdfe23d57864741c8a1e900c5e0cb440648bc4
박지훈 <qkrwlgns0510@gmail.com>
2024-12-07 16:02:11 +0900

    feat : kafka errorhandler

--- src/main/java/com/jhpark/simple_chat_consumer/kafka/config/KafkaConfig.java
+++ src/main/java/com/jhpark/simple_chat_consumer/kafka/config/KafkaConfig.java
@@ -1,8 +1,21 @@
 package com.jhpark.simple_chat_consumer.kafka.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class KafkaConfig {
-    
+
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        return new DefaultErrorHandler(
+                (record, exception) -> log.error("Skipping record due to error: {}", record.value(), exception),
+                new FixedBackOff(0L, 0)
+        );
+    }
 }
