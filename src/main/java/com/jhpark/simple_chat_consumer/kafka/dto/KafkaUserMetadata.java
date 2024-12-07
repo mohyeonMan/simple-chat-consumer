fe46eea90b6b6b13251f2cbe923af5bd538a3429
박지훈 <qkrwlgns0510@gmail.com>
2024-12-07 15:35:29 +0900

    fix : KafkaUserMetadata default constructor required

--- src/main/java/com/jhpark/simple_chat_consumer/kafka/dto/KafkaUserMetadata.java
+++ src/main/java/com/jhpark/simple_chat_consumer/kafka/dto/KafkaUserMetadata.java
@@ -1,10 +1,8 @@
 package com.jhpark.simple_chat_consumer.kafka.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class KafkaUserMetadata {
     private Long userId;
     private String sessionId;
